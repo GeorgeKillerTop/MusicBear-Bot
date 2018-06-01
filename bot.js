d341c6d1a56714796c2e9f3e804a7e66c401b207
GeorgeKillerTop <37047684+GeorgeKillerTop@users.noreply.github.com>
2018-06-01 17:38:58 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,323 @@
+const Discord = require ("discord.js");
+const ytdl = require('ytdl-core');
+const ms = require("ms");
+const YouTube = require('simple-youtube-api');
+const GOOGLE_API_KEY = "AIzaSyDUmo-BtB5oQr5Y3RSgYYBMj9rFKMr-W2s";
+const prefix = "b.";
+const fs = require("fs");
+const youtube = new YouTube(GOOGLE_API_KEY);
+const queue = new Map();
+
+var bot = new Discord.Client();
+var servers = {};
+
+bot.on("ready", function() {
+    console.log("Ready");
+    bot.user.setGame(`on ${bot.guilds.size} servers!`)
+});
+
+bot.on("message", async message => {
+if (message.author.bot) return undefined;
+	if (!message.content.startsWith(prefix)) return undefined;
+
+    const args = message.content.split(' ');
+	const searchString = args.slice(1).join(' ');
+	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+	const serverQueue = queue.get(message.guild.id);
+    let messageArray = message.content.split(" ");
+    let args2 = messageArray.slice(1);
+    var args3 = message.content.substring(prefix.length).split(" ");
+
+    switch (args3[0].toLowerCase()) {
+        case "play":
+        const voiceChannel = message.member.voiceChannel;
+        if (!voiceChannel) {
+            var E31 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "Trebuie sa intri pe un Voice Channel ")
+            .setTimestamp();
+        return message.channel.send(E31);
+        };;
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
+            for (const video of Object.values(videos)) {
+                const video2 = await youtube.getVideoByID(video.id);  
+                await handleVideo(video2, message, voiceChannel, true); 
+            }
+                var E31 = new Discord.RichEmbed()
+                .setColor("#15f153")
+                .addField("✅ Playlist:", `**${playlist.title}** a fost adaugata in playlist`)
+                .setTimestamp();
+            return message.channel.send(E31);
+        } else {
+            try {
+                var video = await youtube.getVideo(url);
+            } catch (error) {
+                try {
+                    var videos = await youtube.searchVideos(searchString, 10);
+                    let index = 0;
+                    var E32 = new Discord.RichEmbed()
+                .setColor("#15f153")
+                .addField("Top 10 videoclipuri gasite:", `${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
+Scire pe chat numarul corespunzator videoclipului pe care vrei sa il asculti `)
+                .setTimestamp();
+                    message.channel.send(E32);
+                    try {
+                        var response = await message.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
+                            maxMatches: 1,
+                            time: 30000,
+                            errors: ['time']
+                        });
+                    } catch (err) {
+                        console.error(err);
+                        var E33 = new Discord.RichEmbed()
+                        .setColor("#15f153")
+                        .addField("Eroare", "Timpul a expiart sau nu ai pus un numar")
+                        .setTimestamp();
+                         return message.channel.send(E33);
+                    }
+                    const videoIndex = parseInt(response.first().content);
+                    var video = await youtube.getVideoByID(videos[videoIndex - 1].id);
+                } catch (err) {
+                    console.error(err);
+                    var E34 = new Discord.RichEmbed()
+                    .setColor("#15f153")
+                    .addField("Eroare", "Mention owener")
+                    .setTimestamp();
+                return message.channel.send(E34);
+                }
+            }
+            return handleVideo(video, message, voiceChannel);
+        }
+            break;
+        case "skip" :
+        const voiceChannel2 = message.member.voiceChannel;
+    if (!voiceChannel2) {
+        var E31 = new Discord.RichEmbed()
+        .setColor("#15f153")
+        .addField("Eroare", "Trebuie sa intri pe un Voice Channel ")
+        .setTimestamp();
+    return message.channel.send(E31);
+    };
+    if (!serverQueue) return message.channel.send('There is nothing playing that I could skip for you.')
+		serverQueue.connection.dispatcher.end('Skip command has been used!')
+        return undefined;
+            break;
+        case "stop" :
+        if (!message.member.voiceChannel) {
+            var E38 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "Trebuie sa intri pe un Voice Channel ")
+            .setTimestamp();
+        return message.channel.send(E38);
+        }
+        if (!serverQueue) {
+            var E40 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "MusicBear nu este pe un Voice Channel")
+            .setTimestamp();
+            return message.channel.send(E40);
+        }
+            serverQueue.songs = [];
+        var server = servers[message.guild.id];
+        if (message.guild.voiceConnection) message.guild.voiceConnection.disconnect();
+                var E39 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("MusicBear s-a deconectat",":x:")
+            .setTimestamp();
+            message.channel.sendMessage(E39);       
+            break;
+        case "volume" :
+        if (!message.member.voiceChannel) {
+            var E41 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "Trebuie sa intri pe un Voice Channel ")
+            .setTimestamp();
+        return message.channel.send(E41);
+        }
+		if (!serverQueue) {
+            var E42 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "MusicBear nu este pe un Voice Channel")
+            .setTimestamp();
+            return message.channel.send(E42);
+        }
+        if (!args[1]) {
+            var E40 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Volum:", `Volumul actiual este **${serverQueue.volume}**`)
+            .setTimestamp();
+            return message.channel.send(E40);
+        }
+		serverQueue.volume = args[1];
+        serverQueue.connection.dispatcher.setVolumeLogarithmic(args[1] / 5);
+        {
+            var E40 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Volum:", `Volumul a fost setat la **${args[1]}**`)
+            .setTimestamp();
+            return message.channel.send(E40);
+        }
+            break;
+        case "now-playing" :
+        if (!serverQueue) {
+            var E44 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "Playlistul e gol")
+            .setTimestamp();
+            return message.channel.send(E44);
+        }
+            var E45 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Now playing:", `**${serverQueue.songs[0].title}**`);
+            return message.channel.send(E45);
+            break;
+        case "playlist" :
+        if (!serverQueue) {
+            var E43 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "Playlistul e gol")
+            .setTimestamp();
+            return message.channel.send(E43);
+        }
+        var E45 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("**Playlist:**", `${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
+            
+**Now playing:** ${serverQueue.songs[0].title}
+                    `);
+            return message.channel.send(E45);
+            break;
+        case "pause" :
+        if (serverQueue && serverQueue.playing) {
+			serverQueue.playing = false;
+            serverQueue.connection.dispatcher.pause();
+                var E47 = new Discord.RichEmbed()
+                .setColor("#15f153")
+                .addField("Pause", ` ${serverQueue.songs[0].title} a fost pus pe pauza`)
+                .setTimestamp();
+                return message.channel.send(E47);
+		}
+            var E46 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "MusicBear nu este pe un Voice Channel")
+            .setTimestamp();
+            return message.channel.send(E46);
+            break;
+        case "resume" :
+        if (serverQueue && !serverQueue.playing) {
+			serverQueue.playing = true;
+			serverQueue.connection.dispatcher.resume();
+            var E48 = new Discord.RichEmbed()
+                .setColor("#15f153")
+                .addField("Resume", `Se continua videoclipul ${serverQueue.songs[0].title}`)
+                .setTimestamp();
+                return message.channel.send(E48);
+		}
+        var E51 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("Eroare", "MusicBear nu este pe un Voice Channel")
+            .setTimestamp();
+            return message.channel.send(E51);
+            break;
+        case "help" :
+             var E8 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("b.help-music", '> detalii despre partea legata de muzica')
+            .addField("Add Fresh on your server", '> https://discordapp.com/api/oauth2/authorize?client_id=452117702233030676&permissions=8&scope=bot')
+            .setTimestamp();
+            message.channel.sendMessage(E8);
+            break;
+        case "help-music" :
+             var E9 = new Discord.RichEmbed()
+            .setColor("#15f153")
+            .addField("b.play ", '> Comanda pentru a asculta muzica')
+            .addField("b.skip", '> Comanda pentru a da skip la urmatoarea pesa din playlist')
+            .addField("b.stop", '> Comanda pentru a opri Muzica')
+            .addField("b.volume", '> Comanda pentru ajusta volumul botului muzica')
+            .addField("b.now-playing", '> Comanda pentru a vedea ce melodie se reda')
+            .addField("b.pause", '> Comanda pentru a pune pe pauza muzica')
+            .addField("b.resume", '> Comanda pentru a relua muzica')
+            .setTimestamp();
+            message.channel.sendMessage(E9);
+            break;
+        default:
+        var T = new Discord.RichEmbed()
+        .setColor("#15f153")
+        .addField("Comanda invalida",'Incearca b.help')
+        message.channel.sendMessage(T); 
+    }
+});
+async function handleVideo(video, msg, voiceChannel, playlist = false) {
+	const serverQueue = queue.get(msg.guild.id);
+	console.log(video);
+	const song = {
+		id: video.id,
+		title: Discord.escapeMarkdown(video.title),
+		url: `https://www.youtube.com/watch?v=${video.id}`
+	};
+	if (!serverQueue) {
+		const queueConstruct = {
+			textChannel: msg.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+		queue.set(msg.guild.id, queueConstruct);
+
+		queueConstruct.songs.push(song);
+
+		try {
+			var connection = await voiceChannel.join();
+			queueConstruct.connection = connection;
+			play(msg.guild, queueConstruct.songs[0]);
+		} catch (error) {
+			console.error(`I could not join the voice channel: ${error}`);
+			queue.delete(msg.guild.id);
+			return msg.channel.send(`I could not join the voice channel: ${error}`);
+		}
+	} else {
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		if (playlist) return undefined;
+        else  {
+        var E35 = new Discord.RichEmbed()
+        .setColor("#15f153")
+        .addField(":white_check_mark: Playlist:", `**${song.title}** a fost adougata in playlist`)
+        .setTimestamp();
+         return msg.channel.send(E35)
+        }
+	}
+	return undefined;
+}
+
+function play(guild, song , message , channel) {
+	const serverQueue = queue.get(guild.id);
+
+	if (!song) {
+		serverQueue.voiceChannel.leave();
+		queue.delete(guild.id);
+		return;
+	}
+	console.log(serverQueue.songs);
+
+	const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+		.on('end', reason => {
+			if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+			else console.log(reason);
+			serverQueue.songs.shift();
+			play(guild, serverQueue.songs[0]);
+		})
+		.on('error', error => console.error(error));
+    dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    
+        var E50 = new Discord.RichEmbed()
+        .setColor("#15f153")
+        .addField("Now Playing", `**${song.title}** `)
+        .setTimestamp();
+        serverQueue.textChannel.send(E50)
+    }   
+bot.login(process.env.BOT_TOKEN);
